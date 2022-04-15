8f09304da4700453cbf04da8db806b8ca2cd6267
Brandnn07 <brandcarter4@gmail.com>
2022-04-15 13:51:12 -0400

    final changes

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 var total = 500
-var newTotal
+// var newTotal
 
 function init () {
-    var input = prompt("Would you like to deposit money or withdraw money? Please say either 'deposit' or 'withdraw'");
+    var input = prompt("Would you like to deposit money or withdraw money? Please say either 'deposit', 'withdraw' or 'total'");
     if ( input === "deposit"){
         var inputD = parseInt(prompt("How much would you like to deposit?"));
-        let newTotal = total + inputD;
+        var newTotal = total + inputD;
         alert(`Your new balance is ${newTotal}`);
         if (newTotal > 0) {
             alert("YOU GOT MONEEYYYYYY! Let me hold some");
@@ -14,13 +14,15 @@ function init () {
         }
     } else if ( input === "withdraw") {
         var inputW = parseInt(prompt("How much would you like to withdraw?"));
-        let newTotal = total - inputW
+        var newTotal = total - inputW
         alert(`Your new balance is ${newTotal}`);
         if (newTotal > 0) {
             alert("YOU GOT MONEEYYYYYY! Let me hold some");
         } else {
             alert("Low funds :(")
         }
+    } else if (input === "total") {
+        alert(total)
     }
     init();
 }
